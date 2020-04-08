36040aea91fb32b2e57881eeb20bf9de3b2be9d6
Daniel Paz <danielpaz601@gmail.com>
2020-04-08 03:43:07 +0300

    Updates

---
+++ precache-manifest.dc9ab0b82281fbdf62980cd2a4528d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d02280dd912f343178ff5503c4b95d0",
+    "url": "/React-Tetris-Game/index.html"
+  },
+  {
+    "revision": "43fa15f8c4c0d3e749ad",
+    "url": "/React-Tetris-Game/static/css/main.e53194f1.chunk.css"
+  },
+  {
+    "revision": "579dd1ed5b33ae0d317f",
+    "url": "/React-Tetris-Game/static/js/2.25300b15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Tetris-Game/static/js/2.25300b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fa15f8c4c0d3e749ad",
+    "url": "/React-Tetris-Game/static/js/main.3d4560fd.chunk.js"
+  },
+  {
+    "revision": "e980fc1ce1b5dcefc079",
+    "url": "/React-Tetris-Game/static/js/runtime-main.9cc6df2a.js"
+  }
+]);
